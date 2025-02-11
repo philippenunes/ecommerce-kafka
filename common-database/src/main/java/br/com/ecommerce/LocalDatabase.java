35a98632218c7f35ad55e2755eaa9ccd95296eca
Philippe Nunes <philippenunes@live.com>
2025-02-10 23:20:07 -0300

    local database and removing config files from git

---
+++ common-database/src/main/java/br/com/ecommerce/LocalDatabase.java
@@ -0,0 +1,37 @@
+package br.com.ecommerce;
+
+import java.sql.*;
+
+public class LocalDatabase {
+
+    private final Connection connection;
+
+    LocalDatabase(String name) throws SQLException {
+        String url = "jdbc:sqlite:" + name + ".db";
+        this.connection = DriverManager.getConnection(url);
+    }
+
+    public void createIfNotExists(String sql) {
+        try {
+            connection.createStatement().execute(sql);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public boolean update(String statement, String... params) throws SQLException {
+        return prepare(statement, params).execute();
+    }
+
+    public ResultSet query(String statement, String... params) throws SQLException {
+        return prepare(statement, params).executeQuery();
+    }
+
+    private PreparedStatement prepare(String statement, String[] params) throws SQLException {
+        var preparedStatement = connection.prepareStatement(statement);
+        for (int i = 0; i < params.length; i++) {
+            preparedStatement.setString(i + 1, params[i]);
+        }
+        return preparedStatement;
+    }
+}
